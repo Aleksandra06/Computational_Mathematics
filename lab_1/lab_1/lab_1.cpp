12796a4a9de384348c3e3dd6e53025780bde4678
Aleksandra06 <plyaskina-sasha@mail.ru>
2019-04-03 21:46:45 +0700

    lab_1 and lab_2

---
+++ lab_1/lab_1/lab_1.cpp
@@ -0,0 +1,126 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int M, N;
+
+void look(float **mas, int n, int m);
+void obmen(float **mas, int a);
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	ifstream file;
+	file.open("matrix.txt");///открываем файл
+	//считывание из файла//
+	file >> N;//число строк
+	M = N + 1;//число столбцов
+	//cout << M << " " << N << endl;;
+	float **matrix = new float*[M];///выделяем память в строке.хз
+	for (int i = 0; i < M; i++) {
+		matrix[i] = new float[N];///выделяем в каждом элементе строки память, получаем столбцы.хз
+	}
+	for (int i = 0; i < N; i++) {///читаем матрицу
+		for (int j = 0; j < M; j++) {
+			if (file.eof()) {///если элементов не хватает, то ошибка
+				cout << "Ошибка чтения файла";
+				return 0;
+			}
+			file >> matrix[j][i];///считываем из файла один элемент типа инт (потому что матрица типа инт) в матрицу
+		}
+	}
+	file.close();///закрываем файл
+	look(matrix, M, N);///смотрим матрицу
+	cout << "Обычный/модернизированный (0/1): " << endl;
+	int r;
+	cin >> r;
+	float k;
+	switch (r) {
+	case 0://обычный
+		for (int j = 0; j < N - 1; j++) {//смотри комментарии ниже. здесь так же, но без обмена
+			if (matrix[j][j] == 0) {//вис ис костыль, хотя может так и должно быть. ХЗ. Если эл. = 0, то получается фигня, мы просто не можем в этом столбце другие строки обратить в ноль
+				float tmp;
+				int a = j;
+				while (matrix[j][a] == 0 && a < N) {
+					a++;
+				}
+				for (int i = j; i < M; i++) {///меняем местами строку а и строку мах местами
+					tmp = matrix[i][j + 1];
+					matrix[i][j + 1] = matrix[i][j];
+					matrix[i][j] = tmp;
+				}
+			}
+			for (int i = j + 1; i < N; i++) {
+				k = matrix[j][i] / matrix[j][j];
+				cout << k << endl;
+				for (int e = j; e < M; e++) {
+					matrix[e][i] = matrix[e][i] - matrix[e][j] * k;
+				}
+			}
+			look(matrix, M, N);
+		}
+		break;
+	case 1://модернизированный
+		for (int j = 0; j < N - 1; j++) {// пробегаем по всем строкам, исключая последнюю
+			obmen(matrix, j);//поднимаем наверх строку с макс. эл. в j строке
+			//look(matrix, M, N); cout << endl;
+			for (int i = j + 1; i < N; i++) {//идем по всем строкам, исключая первую
+				k = matrix[j][i] / matrix[j][j];//вычисляем коэффициент
+				cout << k << endl;
+				if (k == 0) break;
+				else {
+					for (int e = j; e < M; e++) {//от i строки отнимаем j умноженную на коэф.
+						matrix[e][i] = matrix[e][i] - matrix[e][j] * k;
+					}
+				}
+			}
+			look(matrix, M, N);//смотрим, что получилось
+		}
+		break;
+	}
+	////решение системы уравнений//
+	float *x;
+	float tmp;
+	x = new float[N];
+	for (int i = 1; i <= N; i++) {//пробегаем по всем строчкам
+		tmp = 0;
+		for (int j = 0; j < i - 1; j++) {//ищем сумму известных х умноженных на коэффициент
+			tmp = tmp + x[N - (j + 1)] * matrix[N - (j + 1)][N - i];
+		}
+		x[N - i] = (matrix[M - 1][N - i] - tmp) / matrix[N - i][N - i];//нахождение искомого x: ах+b=c; x=(c-b)/a
+	}
+	////Ответ//
+	cout << "Ответ:" << endl;
+	for (int i = 0; i < N; i++) {
+		cout << "x" << i << "= " << x[i] << endl;
+	}
+	system("PAUSE");
+	return 0;
+}
+
+void look(float **mas, int n, int m) {//вывод матрицы на экран
+	cout << endl;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++)
+			cout << mas[j][i] << "\t ";
+		cout << endl;
+	}
+}
+
+void obmen(float **mas, int a) {//поднятие строки матрицы с максимальным элементом в заданном столбце, наверх.
+	float tmp;
+	int max;
+	max = a;
+	for (int j = a + 1; j < N; j++) { ///ищем максимальный элемент в заданном столбце а
+		if (abs(mas[a][max]) < abs(mas[a][j]))///костыль, потому что если макс. эл. = 0, то получается фигня, мы просто не можем в этом столбце другие строки обратить в ноль
+			max = j;
+	}
+	for (int i = a; i < M; i++) {///меняем местами строку а и строку мах местами
+		tmp = mas[i][max];
+		mas[i][max] = mas[i][a];
+		mas[i][a] = tmp;
+	}
+}
---
+++ lab_2/lab_2/lab_2.cpp
@@ -0,0 +1,252 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+int M, N;//кол-во строк и столбцов
+const double eps = 0.001;
+double **matrix;
+double **A;
+double **C;
+double *B;
+double *Cx;
+double *Cx1;
+double *Cx2;
+double BeskNormB;
+double BeskNormC;
+
+void look(double **, int a, int b);
+void build_A();
+void build_B();
+void build_C();
+double calc_BeskNormB();
+double calc_BeskNormC();
+double Kolchshag();
+int iter(double **);
+void CX(double *, double **, int);
+
+void Zeidel(double **, int );
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	//считывание из файла//
+	ifstream file;
+	file.open("matrix.txt");
+	if (file.eof()) {
+		cout << "Ошибка чтения файла";
+		return 0;
+	}
+	file >> M;//число строк
+	N = M + 1;//число столбцов
+	matrix = new double*[N];
+	int tmp;
+	for (int i = 0; i < N; i++) {
+		matrix[i] = new double[M];
+	}
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			if (file.eof()) {
+				cout << "Ошибка чтения файла";
+				return 0;
+			}
+			file >> tmp;
+			matrix[j][i] = (int)tmp;
+		}
+	}
+	file.close();
+	look(matrix, M, N);
+	cout << endl;
+	//
+	cout << "Матрица А:" << endl;
+	A = new double*[N];
+	for (int i = 0; i < M; i++) {
+		A[i] = new double[M];
+	}
+	build_A();
+	look(A, M, M);
+	cout << endl;
+	cout << " Матрица В:" << endl;
+	B = new double[M];
+	build_B();
+	cout << endl;
+	cout << " Матрица C:" << endl;
+	C = new double*[N];
+	for (int i = 0; i < M; i++) {
+		C[i] = new double[M];
+	}
+	build_C();
+	look(C, M, M);
+	cout << endl;
+	//
+	BeskNormB = calc_BeskNormB();
+	BeskNormC = calc_BeskNormC();
+	cout << "||B|| = " << BeskNormB << endl;
+	cout << "||C|| = " << BeskNormC << endl;
+	double n;
+	n = Kolchshag();
+	cout << "Количество шагов: " << n << endl;
+	cout << "Для точности: " << eps << endl;
+	//
+	int k;
+	k = (int)n + 1;
+	int fl;
+	cout << "МПИ/Зельделя (0/1): ";
+	cin >> fl;
+
+	double **X;
+	switch (fl) {
+	case 0:
+		X = new double*[M];
+		for (int i = 0; i < M; i++) {
+			X[i] = new double[k + 1];
+		}
+		for (int i = 0; i < M; i++) {
+			X[i][0] = 0;
+		}
+		k = iter(X);
+
+		for (int i = 0; i < k; i++) {
+			cout << "X" << i << "= ( ";
+			for (int j = 0; j < M; j++) {
+				cout << "\t" << X[j][i];
+			}
+			cout << " )" << endl;
+		}
+		break;
+	case 1:
+		X = new double*[M];
+		for (int i = 0; i < M; i++) {
+			X[i] = new double[k + 1];
+		}
+		Cx1 = new double[M];
+		Cx2 = new double[M];
+		Cx = new double[M];
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < k + 1; j++)
+				X[i][j] = 0;
+		}
+		int i = 0;
+		do {
+			i++;
+			Zeidel(X, i);
+			cout << "Шаг " << i << " :" << endl;
+			for (int t = 0; t < M; t++) {
+				cout << "X[" << t + 1 << "]= " << X[t][i] << endl;
+			}
+		} while (abs(abs(X[0][i]) - abs(X[0][i - 1])) > eps);
+		break;
+	}
+	system("PAUSE");
+	return 0;
+}
+
+
+void look(double **mass, int a, int b) {//вывод матрицы на экран
+	cout << endl;
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j < b; j++)
+			cout << mass[j][i] << "\t ";
+		cout << endl;
+	}
+}
+
+void build_A() {
+	double k;
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < M; j++) {
+			k = 1 / matrix[i][i];
+			A[j][i] = matrix[j][i] * k;
+		}
+	}
+}
+
+void build_B() {
+	for (int i = 0; i < M; i++) {
+		{
+			B[i] = matrix[M][i] / matrix[i][i];
+			cout << B[i];
+		}
+		cout << endl;
+	}
+}
+
+void build_C() {
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < M; j++) {
+			if (i == j)
+				C[j][i] = 0;
+			else
+				C[j][i] = A[j][i];
+		}
+	}
+}
+
+double calc_BeskNormB() {
+	double max = abs(B[0]);
+	for (int i = 1; i < M; i++) {
+		if (max < abs(B[i]))
+			max = abs(B[i]);
+	}
+	return max;
+}
+
+double calc_BeskNormC() {
+	double max = 0, tmp;
+	for (int i = 0; i < M; i++) {
+		tmp = 0;
+		for (int j = 0; j < M; j++) {
+			tmp = tmp + abs(C[j][i]);
+		}
+		if (max < tmp) max = tmp;
+	}
+	return max;
+}
+
+double Kolchshag() {
+	double podlog, otnoshlog, kolsh;
+	podlog = (eps*(1 - BeskNormC)) / BeskNormB;
+	otnoshlog = log(podlog) / log(BeskNormC);
+	kolsh = otnoshlog + 1;
+	return kolsh;
+}
+
+void CX(double *cx, double **X, int i) {
+	for (int m = 0; m < M; m++)
+	{
+		cx[m] = 0;
+		for (int k = 0; k < M; k++) {
+			cx[m] = cx[m] + X[k][i] * C[m][k];
+		}
+	}
+}
+
+int iter(double **X) {
+	double *cx;
+	cx = new double[M];
+	int i = 0;
+	do {
+		i++;
+		CX(cx, X, i-1);
+		for (int j = 0; j < M; j++) {
+			X[j][i] = B[j] - cx[j];
+			/*cout << X[j][i] << endl;
+			cout << X[0][i] - X[0][i - 1] << endl;*/
+		}
+		//cout << "X-X " << abs(X[0][i]) - abs(X[0][i - 1]) << endl;
+	} while (abs(abs(X[0][i]) - abs(X[0][i - 1])) > eps);
+	return i;
+}
+
+
+void Zeidel(double **X, int i) {
+	for (int l = 0; l < M; l++) {
+		for (int j = 0; j < M; j++) {
+			X[l][i] = X[l][i] + X[j][i - 1] * C[l][j];
+		}
+		X[l][i] = B[l] - X[l][i];
+	}
+}
