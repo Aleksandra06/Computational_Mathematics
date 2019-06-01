c8c0130d5018959f728205e09ff74f0c05a5f9b2
Aleksandra06 <plyaskina-sasha@mail.ru>
2019-06-01 17:09:32 +0700

    lab_7d2

--- lab_7/lab_7/lab_7.cpp
+++ lab_7/lab_7/lab_7.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-const double eps = 0.00001;
+const double eps = 0.00000001;
 const double Y = 1;
 const double Yp = 1;
 const double H = 0.2;
@@ -13,11 +13,12 @@ const double a = 0;
 const double b = 1;
 
 double fpp(double, double, double);
-void euler();
-void RK2t();
-void RK2p();
-void RK4();
-void milna();
+double f(double x, double y, double z);
+double g(double x, double y, double z);
+double RK4(double h, int range, double yy[], double z0);
+double RK4DP(double m);
+double RK2t(double h, int range, double yy[], double z0);
+double RK2tDP();
 
 int main()
 {
@@ -25,32 +26,20 @@ int main()
 	int flag = 10;
 	while (flag != 0) {
 		system("cls");
-		cout << "0)Выход\n1) Эйлер \n2) Рунге-Кутта 2 порядка с усреднением по времени\n3) Рунге-Кутта 2 порядка с усреднением по произвоной\n4) Рунге - Кутта 4 порядка\n5) Милна" << endl;
+		cout << "0)Выход\n1) Рунге-Кутта 2 порядка с усреднением по времени\n2) Рунге - Кутта 4 порядка\n" << endl;
 		cout << "-------------------------------------------------------" << endl;
 		cout << "Метод: ";
 		cin >> flag;
 		switch (flag)
 		{
-		/*case 1:
-			euler();
+		case 1:
+			RK2tDP();
 			_getch();
-			break;*/
+			break;
 		case 2:
-			RK2t();
+			RK4DP(Yp);
 			_getch();
 			break;
-		case 3:
-			RK2p();
-			_getch();
-			break;
-		case 4:
-			RK4();
-			_getch();
-			break;
-		/*case 5:
-			milna();
-			_getch();
-			break;*/
 		default:
 			break;
 		}
@@ -64,428 +53,93 @@ double fpp(double x, double y, double yp) {
 	return (double)(exp(x) + y) / 2;
 }
 
-void euler() {
-	double **f, **fl;
-	f = new double*[2];
-	fl = new double*[2];
-	double x = a;
-	double y = Y;
-	double yp = Yp;
-	double h = H;
-	bool toch = 1;
-	int k = (int)(abs(b - a) / h) + r;
-	for (int j = 0; j < 2; j++) {
-		f[j] = new double[k];
-		fl[j] = new double[k];
+double RK2t(double h, int r, double yy[], double z0) {
+	double xc = a, yc = Y, zc = z0, y1c = 1, k1, k2, k4, k3, k11, k22, k44, k33;
+	for (int i = 0; i < r; i++) {
+		printf("%0.8f\t%0.8f\t%0.8f\t\n", xc, y1c, zc);
+		yy[i] = yc + (double)h / 2 * zc;
+		zc = zc + (double)h / 2 * fpp(xc + h / 2.0, yc, zc);
+		yc = y1c;
+		xc += h;
 	}
-	for (int j = 0; j < k; j++) {
-		f[0][j] = y + h * yp;
-		f[1][j] = yp + h * fpp(x, y, yp);
-		y = f[0][j];
-		yp = f[1][j];
-		x += h;
-	}
-	int i = 1;
-	do {
-		cout << endl;
-		//cout << "Шаг " << i << endl;
-		//cout << "h = " << h << endl;
-		printf("%0.8f\t\t%0.8f\t\t%0.8f\n\n", a + h, f[0][k - 1], f[1][k - 1]);
-		i++;
-		k = (int)(abs(b - a) / h) + r;
-		for (int j = 0; j < 2; j++) {
-			delete[] fl[j];
-		}
-		for (int j = 0; j < 2; j++) {
-			fl[j] = new double[k];
-		}
-		for (int j = 0; j < k; j++) {
-			fl[0][j] = f[0][j];
-			fl[1][j] = f[1][j];
-		}
-		for (int j = 0; j < 2; j++) {
-			delete[]f[j];
-		}
-		h = h / 2;
-		k = (int)(abs(b - a) / h) + r;
-		for (int j = 0; j < 2; j++) {
-			f[j] = new double[k];
-		}
-		x = a;
-		y = Y;
-		yp = Yp;
-		for (int j = 0; j < k; j++) {
-			f[0][j] = y + h * yp;
-			f[1][j] = yp + h * fpp(x, y, yp);
-			y = f[0][j];
-			yp = f[1][j];
-			//printf("%0.8f\t\t%0.8f\t\t%0.8f\n\n", x, f[0][k - 1], f[1][k - 1]);
-			x += h;
-		}
-		//k = (int)(abs(b - a) / (h * 2)) + r;
-		//cout << "Y(h)=(" << fl[0][k - 1] << "," << fl[1][k - 1] << ")" << endl;
-		//k = (int)(abs(b - a) / h) + r;
-		//cout << "Y(h/2)=(" << f[0][k - 1] << "," << f[1][k - 1] << ")" << endl;
-		k = (int)(abs(b - a) / (h * 2)) + r;
-		for (int j = 1; j < k; j++) {
-			if (abs(f[0][2 * j] - fl[0][j]) <= 3 * eps) toch = 0;
-			else toch = 1;
-		}
-	} while (abs(f[0][2 * k - 1] - fl[0][k - 1]) > 3 * eps);
+	printf("%0.8f\t%0.8f\t%0.8f\t\n", xc, y1c, zc);
+	return y1c;
 }
 
-void RK2t() {
-	double **f, **fl, **fz;
-	f = new double*[2];
-	fl = new double*[2];
-	fz = new double*[2];
-	double x = a;
-	double y = Y;
-	double yp = Yp;
-	double h = H;
-	bool toch = 1;
-	int k = (int)(abs(b - a) / h) + r;
-	for (int j = 0; j < 2; j++) {
-		f[j] = new double[k];
-		fl[j] = new double[k];
-		fz[j] = new double[k];
-	}
-	for (int j = 0; j < k; j++) {
-		fz[0][j] = y + (double)h / 2 * yp;
-		fz[1][j] = yp + (double)h / 2 * fpp(x, y, yp);
-		f[0][j] = y + h * fz[1][j];
-		f[1][j] = yp + h * fpp(x + (double)h / 2, fz[0][j], fz[1][j]);
-		y = f[0][j];
-		yp = f[1][j];
-		x += h;
-	}
-	int i = 1;
+double RK2tDP() {
+	int r, R;
+	double xn = b, x0 = a, m = Yp;
+	double *yy, *YY;
+	double H = 0.2, h = H / 2;
+	cout << "       X\t\tY\t\tY'" << endl;
+	double result;
 	do {
+		R = (int)((xn - x0) / h);
+		r = (int)((xn - x0) / H);
+		YY = new double[r];
+		cout << "H = " << H << endl;
+		RK4(H, r, YY, m);
+		yy = new double[R];
+		cout << "h = " << h << endl;
+		result = RK4(h, R, yy, m);
 		cout << endl;
-		//cout << "Шаг " << i << endl;
-		//cout << "h = " << h << endl;
-		k = (int)(abs(b - a) / h) + r;
-		//cout << "Yz(h)=(" << fz[0][k - 1] << "," << fz[1][k - 1] << ")" << endl;
-		//cout << "Y(h)=(" << f[0][k - 1] << "," << f[1][k - 1] << ")" << endl;
-		//printf("%0.10f\t\t%0.10f\t\t%0.10f\n\n", a + h, f[0][k - 1], f[1][k - 1]);
-		i++;
-		k = (int)(abs(b - a) / h) + r;
-		for (int j = 0; j < 2; j++) {
-			delete[] fl[j];
-		}
-		for (int j = 0; j < 2; j++) {
-			fl[j] = new double[k];
-		}
-		for (int j = 0; j < k; j++) {
-			fl[0][j] = f[0][j];
-			fl[1][j] = f[1][j];
-		}
-		for (int j = 0; j < 2; j++) {
-			delete[]f[j];
-			delete[]fz[j];
-		}
-		h = h / 2;
-		x = a;
-		y = Y;
-		yp = Yp;
-		k = (int)(abs(b - a) / h) + r;
-		for (int j = 0; j < 2; j++) {
-			f[j] = new double[k];
-			fz[j] = new double[k];
-		}
-		for (int j = 0; j < k; j++) {
-			fz[0][j] = y + (double)h / 2 * yp;
-			fz[1][j] = yp + (double)h / 2 * fpp(x, y, yp);
-			f[0][j] = y + h * fz[1][j];
-			f[1][j] = yp + h * fpp(x + (double)h / 2, fz[0][j], fz[1][j]);
-			y = f[0][j];
-			yp = f[1][j];
-			/*if (x > 0.99670)
-				printf("1\t\t%0.10f\t\t%0.10f\n\n", f[0][j], f[1][j]);
-			else*/
-				printf("%0.10f\t\t%0.10f\t\t%0.10f\n\n", x, f[0][j], f[1][j]);
-			x += h;
-		}
-		/*cout << "Yz(h/2)=(" << fz[0][k - 1] << "," << fz[1][k - 1] << ")" << endl;
-		cout << "Y(h/2)=(" << f[0][k - 1] << "," << f[1][k - 1] << ")" << endl;*/
-		k = (int)(abs(b - a) / (h * 2)) + r;
-		for (int j = 1; j < k; j++) {
-			if (abs(f[0][2 * j] - fl[0][j]) <= 3 * eps) toch = 0;
-			else toch = 1;
-		}
-	} while (abs(f[0][2 * k - 1] - fl[0][k - 1]) > 3 * eps);
+		h *= 0.5;
+		H = 2 * h;
+	} while (abs(yy[R - 1] - YY[r - 1]) > eps);
+	h = 0.1; H = 2 * h;
+	return result;
 }
 
-void RK2p() {
-	double **f, **fl, **fz;
-	f = new double*[2];
-	fl = new double*[2];
-	fz = new double*[2];
-	double x = a;
-	double y = Y;
-	double yp = Yp;
-	double h = H;
-	bool toch = 1;
-	int k = (int)(abs(b - a) / h) + r;
-	for (int j = 0; j < 2; j++) {
-		f[j] = new double[k];
-		fl[j] = new double[k];
-		fz[j] = new double[k];
-	}
-	for (int j = 0; j < k; j++) {
-		fz[0][j] = y + (double)h / 2 * yp;
-		fz[1][j] = yp + (double)h / 2 * fpp(x, y, yp);
-		f[0][j] = y + (double)h / 2 * (yp + fz[1][j]);
-		f[1][j] = yp + (double)h/2*(fpp(x, y, yp) + fpp(x + h, fz[0][j], fz[1][j]));
-		y = f[0][j];
-		yp = f[1][j];
-		x += h;
-	}
-	int i = 1;
+double RK4DP(double m) {
+	int r, R;
+	double xn = b, x0 = a;
+	double *yy, *YY; 
+	double H = 0.2, h = H / 2;
+	cout << "       X\t\tY\t\tY'" << endl;
+	double result;
 	do {
+		R = (int)((xn - x0) / h);
+		r = (int)((xn - x0) / H);
+		YY = new double[r];
+		cout << "H = " << H << endl;
+		RK4(H, r, YY, m);
+		yy = new double[R];
+		cout << "h = " << h << endl;
+		result = RK4(h, R, yy, m);
 		cout << endl;
-		///cout << "Шаг " << i << endl;
-		//cout << "h = " << h << endl;
-		k = (int)(abs(b - a) / h) + r;
-		//cout << "Yz(h)=(" << fz[0][k - 1] << "," << fz[1][k - 1] << ")" << endl;
-		//cout << "Y(h)=(" << f[0][k - 1] << "," << f[1][k - 1] << ")" << endl;
-		//printf("%0.10f\t\t%0.10f\t\t%0.10f\n\n", a + h, f[0][k - 1], f[1][k - 1]);
-		i++;
-		k = (int)(abs(b - a) / h) + r;
-		for (int j = 0; j < 2; j++) {
-			delete[] fl[j];
-		}
-		for (int j = 0; j < 2; j++) {
-			fl[j] = new double[k];
-		}
-		for (int j = 0; j < k; j++) {
-			fl[0][j] = f[0][j];
-			fl[1][j] = f[1][j];
-		}
-		for (int j = 0; j < 2; j++) {
-			delete[]f[j];
-			delete[]fz[j];
-		}
-		h = h / 2;
-		x = a;
-		y = Y;
-		yp = Yp;
-		k = (int)(abs(b - a) / h) + r;
-		for (int j = 0; j < 2; j++) {
-			f[j] = new double[k];
-			fz[j] = new double[k];
-		}
-		for (int j = 0; j < k; j++) {
-			fz[0][j] = y + (double)h / 2 * yp;
-			fz[1][j] = yp + (double)h / 2 * fpp(x, y, yp);
-			f[0][j] = y + (double)h / 2 * (yp + fz[1][j]);
-			f[1][j] = yp + (double)h / 2 * (fpp(x, y, yp) + fpp(x + h, fz[0][j], fz[1][j]));
-			y = f[0][j];
-			yp = f[1][j];
-			//printf("%0.10f\t\t%0.10f\t\t%0.10f\n\n", x, f[0][j], f[1][j]);
-			x += h;
-		}
-		//cout << "Yz(h/2)=(" << fz[0][k - 1] << "," << fz[1][k - 1] << ")" << endl;
-		//cout << "Y(h/2)=(" << f[0][k - 1] << "," << f[1][k - 1] << ")" << endl;
-		k = (int)(abs(b - a) / (h * 2)) + r;
-		for (int j = 1; j < k; j++) {
-			if (abs(f[0][2 * j] - fl[0][j]) <= 3 * eps) toch = 0;
-			else toch = 1;
-		}
-	} while (abs(f[0][2 * k - 1] - fl[0][k - 1]) > 3 * eps);
+		h *= 0.5;
+		H = 2 * h;
+	} while (abs(yy[R - 1] - YY[r - 1]) > eps);
+	h = 0.1; H = 2 * h;
+	return result;
 }
 
-void RK4() {
-	double **f, **fl;
-	f = new double*[2];
-	fl = new double*[2];
-	double x = a;
-	double y = Y;
-	double yp = Yp;
-	double h = H;
-	double k1[2], k2[2], k3[2], k4[2];
-	bool toch = 1;
-	int k = (int)(abs(b - a) / h) + r;
-	for (int j = 0; j < 2; j++) {
-		f[j] = new double[k];
-		fl[j] = new double[k];
-	}
-	for (int j = 0; j < k; j++) {
-		k1[0] = yp;
-		k1[1] = fpp(x, y, yp);
-		k2[0] = yp + (double)h / 2 * k1[1];
-		k2[1] = fpp(x + (double)h / 2, y + (double)h / 2 * k1[0], yp + (double)h / 2 * k1[1]);
-		k3[0] = yp + (double)h / 2 * k2[1];
-		k3[1] = fpp(x + (double)h / 2, y + (double)h / 2 * k2[0], yp + (double)h / 2 * k2[1]);
-		k4[0] = yp + h * k3[1];
-		k4[1] = fpp(x + h, y + h * k3[0], yp + h * k3[1]);
-		f[0][j] = y + (double)h / 6 * (k1[0] + 2 * k2[0] + 2 * k3[0] + k4[0]);
-		f[1][j] = yp + (double)h / 6 * (k1[1] + 2 * k2[1] + 2 * k3[0] + k4[1]);
-		y = f[0][j];
-		yp = f[1][j];
-		x += h;
-	}
-	int i = 1;
-	do {
-		cout << endl;
-		//cout << "Шаг " << i << endl;
-		///cout << "h = " << h << endl;
-		k = (int)(abs(b - a) / h) + r;
-		/*cout << "k(1)=(" << k1[0] << "," << k1[1] << ")" << endl;
-		cout << "k(2)=(" << k2[0] << "," << k2[1] << ")" << endl;
-		cout << "k(3)=(" << k3[0] << "," << k3[1] << ")" << endl;
-		cout << "k(4)=(" << k4[0] << "," << k4[1] << ")" << endl;
-		cout << "Y(h)=(" << f[0][k - 1] << "," << f[1][k - 1] << ")" << endl;*/
-		printf("%0.10f\t\t%0.10f\t\t%0.10f\n\n", a + h, f[0][k - 1], f[1][k - 1]);
-		i++;
-		k = (int)(abs(b - a) / h) + r;
-		for (int j = 0; j < 2; j++) {
-			delete[] fl[j];
-		}
-		for (int j = 0; j < 2; j++) {
-			fl[j] = new double[k];
-		}
-		for (int j = 0; j < k; j++) {
-			fl[0][j] = f[0][j];
-			fl[1][j] = f[1][j];
-		}
-		for (int j = 0; j < 2; j++) {
-			delete[]f[j];
-		}
-		h = h / 2;
-		x = a;
-		y = Y;
-		yp = Yp;
-		k = (int)(abs(b - a) / h) + r;
-		for (int j = 0; j < 2; j++) {
-			f[j] = new double[k];
-		}
-		for (int j = 0; j < k; j++) {
-			k1[0] = yp;
-			k1[1] = fpp(x, y, yp);
-			k2[0] = yp + (double)h / 2 * k1[1];
-			k2[1] = fpp(x + (double)h / 2, y + (double)h / 2 * k1[0], yp + (double)h / 2 * k1[1]);
-			k3[0] = yp + (double)h / 2 * k2[1];
-			k3[1] = fpp(x + (double)h / 2, y + (double)h / 2 * k2[0], yp + (double)h / 2 * k2[1]);
-			k4[0] = yp + h * k3[1];
-			k4[1] = fpp(x + h, y + h * k3[0], yp + h * k3[1]);
-			f[0][j] = y + (double)h / 6 * (k1[0] + 2 * k2[0] + 2 * k3[0] + k4[0]);
-			f[1][j] = yp + (double)h / 6 * (k1[1] + 2 * k2[1] + 2 * k3[0] + k4[1]);
-			y = f[0][j];
-			yp = f[1][j];
-			printf("%0.10f\t\t%0.10f\t\t%0.10f\n\n", x, f[0][j], f[1][j]);
-			x += h;
-		}
-		k = (int)(abs(b - a) / (h * 2)) + r;
-		/*cout << "k(1)=(" << k1[0] << "," << k1[1] << ")" << endl;
-		cout << "k(2)=(" << k2[0] << "," << k2[1] << ")" << endl;
-		cout << "k(3)=(" << k3[0] << "," << k3[1] << ")" << endl;
-		cout << "k(4)=(" << k4[0] << "," << k4[1] << ")" << endl;
-		cout << "Y(h/2)=(" << f[0][k - 1] << "," << f[1][k - 1] << ")" << endl;*/
-		/*for (int j = 1; j < k; j++) {
-			if (abs(f[0][2 * j] - fl[0][j]) <= 15 * eps) toch = 0;
-			else toch = 1;
-		}*/
-	} while (abs(f[0][2 * k - 1] - fl[0][k-1]) > 15 * eps);
+double f(double x, double y, double z) {
+	double result = (exp(x) + y) / 2;
+	return result;
 }
 
-void milna() {
-	double **f, **fl, *fz;;
-	f = new double*[2];
-	fl = new double*[2];
-	fz = new double[2];
-	double x = a;
-	double y = Y;
-	double yp = Yp;
-	double h = H;
-	bool toch = 1;
-	int k = (int)(abs(b - a) / h) + r;
-	for (int j = 0; j < 2; j++) {
-		f[j] = new double[k];
-		fl[j] = new double[k];
+double g(double x, double y, double z) {
+	return (z);
+}
+
+double RK4(double h, int r, double yy[], double z0) {
+	double xc = a, yc = Y, zc = z0, y1c = 1, k1, k2, k4, k3, k11, k22, k44, k33;
+	for (int i = 0; i < r; i++) {
+		printf("%0.8f\t%0.8f\t%0.8f\t\n", xc, y1c, zc);
+		k1 = h * f(xc, yc, zc);
+		k11 = h * g(xc, yc, zc);
+		k2 = h * f(xc + h / 2.0, yc + k11 / 2.0, zc + k1 / 2.0);
+		k22 = h * g(xc + h / 2.0, yc + k11 / 2.0, zc + k1 / 2.0);
+		k3 = h * f(xc + h / 2.0, yc + k22 / 2.0, zc + k2 / 2.0);
+		k33 = h * g(xc + h / 2.0, yc + k22 / 2.0, zc + k2 / 2.0);
+		k4 = h * f(xc + h, yc + k33, zc + k3);
+		k44 = h * g(xc + h, yc + k33, zc + k3);
+		zc = zc + (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0;//y'
+		yy[i] = y1c = yc + (k11 + 2.0 * k22 + 2.0 * k33 + k44) / 6.0;//y
+		yc = y1c;
+		xc += h;
 	}
-	for (int j = 0; j < 3; j++) {
-		f[0][j] = y + h * yp;
-		f[1][j] = yp + h * fpp(x, y, yp);
-		y = f[0][j];
-		yp = f[1][j];
-		x  = x+ h;
-		//cout << "Yh(" << j + 1 << ")=(" << f[0][j] << "," << f[1][j] << ")" << endl;
-	}
-	for (int j = 2; j < k - 1; j++) {
-		fz[0] = f[0][j - 2] + 4 * (double)h / 3 * (2 * f[1][j - 2] - f[1][j - 1] + 2 * f[1][j]);
-		fz[1] = f[1][j - 2] + 4 * (double)h / 3 * (2 * fpp(x - 2 * h, f[0][j - 2], f[1][j - 2]) - fpp(x - 2 * h, f[0][j - 1], f[1][j - 1]) + 2 * fpp(x - h, f[0][j], f[1][j]));
-		f[0][j + 1] = f[0][j - 1] + (double)h / 3 * (f[1][j - 1] - 4 * f[1][j] + fz[1]);
-		f[1][j + 1] = f[1][j - 1] + (double)h / 3 * (fpp(x - 1 * h, f[0][j - 1], f[1][j - 1]) - 4 * fpp(x - 1 * h, f[0][j], f[1][j]) + fpp(x + h, fz[0], fz[1]));
-		y = f[0][j];
-		yp = f[1][j];
-		//printf("%0.8f       %0.8f       %0.8f\n\n", x, f[0][k - 1], f[1][k - 1]);
-		//cout << x << "\t\t" << f[0][k - 1] << "\t\t" << f[1][k - 1] << endl;
-		x =x+h;
-
-		//cout << "!!!" << x << endl;/*
-		//cout << "Yh(" << j + 2 << ")=(" << f[0][j] << "," << f[1][j] << ")" << endl;*/
-	}
-
-	int i = 1;
-	do {
-		cout << endl;
-		//cout << "Шаг " << i << endl;
-		//cout << "h = " << h << endl;
-		k = (int)(abs(b - a) / h) + r;
-		//cout << "Y(h)=(" << f[0][k - 1] << "," << f[1][k - 1] << ")" << endl;
-		//cout << x << "\t\t" << f[0][k - 1] << "\t\t" << f[1][k - 1] << endl;
-		//printf("%0.8f\t\t%0.8f\t\t%0.8f\n", x, f[0][k - 1], f[1][k - 1]);
-		printf("%0.8f\t\t%0.8f\t\t%0.8f\n\n", a + h, f[0][k - 1], f[1][k - 1]);
-		i++;
-		for (int j = 0; j < 2; j++) {
-			delete[] fl[j];
-		}
-		for (int j = 0; j < 2; j++) {
-			fl[j] = new double[k];
-		}
-		for (int j = 0; j < k; j++) {
-			fl[0][j] = f[0][j];
-			fl[1][j] = f[1][j];
-		}
-		for (int j = 0; j < 2; j++) {
-			delete[]f[j];
-		}
-		h = h / 2;
-		x = a;
-		y = Y;
-		yp = Yp;
-		k = (int)(abs(b - a) / h) + r;
-		for (int j = 0; j < 2; j++) {
-			f[j] = new double[k];
-		}
-		for (int j = 0; j < 3; j++) {
-			f[0][j] = y + h * yp;
-			f[1][j] = yp + h * fpp(x, y, yp);
-			y = f[0][j];
-			yp = f[1][j];
-			x = x + h;
-			//cout << "Yh(" << j + 1 << ")=(" << f[0][j] << "," << f[1][j] << ")" << endl;
-		}
-		for (int j = 2; j < k - 1; j++) {
-
-			fz[0] = f[0][j - 2] + 4 * (double)h / 3 * (2 * f[1][j - 2] - f[1][j - 1] + 2 * f[1][j]);
-			fz[1] = f[1][j - 2] + 4 * (double)h / 3 * (2 * fpp(x - 2 * h, f[0][j - 2], f[1][j - 2]) - fpp(x - 2 * h, f[0][j - 1], f[1][j - 1]) + 2 * fpp(x - h, f[0][j], f[1][j]));
-			f[0][j + 1] = f[0][j - 1] + (double)h / 3 * (f[1][j - 1] - 4 * f[1][j] + fz[1]);
-			f[1][j + 1] = f[1][j - 1] + (double)h / 3 * (fpp(x - 1 * h, f[0][j - 1], f[1][j - 1]) - 4 * fpp(x - 1 * h, f[0][j], f[1][j]) + fpp(x + h, fz[0], fz[1]));
-			y = f[0][j];
-			yp = f[1][j];
-			//printf("%0.8f\t\t%0.8f\t\t%0.8f\n\n", x, f[0][k - 1], f[1][k - 1]);
-			//cout << x << "\t\t" << f[0][k - 1] << "\t\t" << f[1][k - 1] << endl;
-			x =x+ h;/*
-			cout << "Yh(" << j + 2 << ")=(" << f[0][j] << "," << f[1][j] << ")" << endl;*/
-
-		}
-		k = (int)(abs(b - a) / (h * 2)) + r;
-		//cout << "Y(h/2)=(" << f[0][k-1] << "," << f[1][k-1] << ")" << endl;
-		for (int j = 1; j < k; j++) {
-			if (abs(f[0][2 * j] - fl[0][j]) <= 3 * eps) toch = 0;
-			else toch = 1;
-		}
-	} while (abs(f[0][2 * k - 1] - fl[0][k - 1]) > 15 * eps);
+	printf("%0.8f\t%0.8f\t%0.8f\t\n", xc, y1c, zc);
+	return y1c;
 }
